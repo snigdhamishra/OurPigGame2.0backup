a8de5f1ea7b4f0a1afa99c0297d86f30ff40709e
Snigdha Mishra <snigdha.mishra.066@k12.friscoisd.org>
2019-10-03 13:27:16 -0500

    diceRoll

--- OurPigGame2.0/ViewController.swift
+++ OurPigGame2.0/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var alternatePlayersButton: UISegmentedControl!
+    @IBOutlet weak var numberRolledLabel: UILabel!
+    
+    @IBOutlet weak var rollDiceButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func rollDiceButtonClicked(_ sender: Any) {
+        let numberRolled = Int.random(in: 1...6)
+        numberRolledLabel.text = "Player rolled a \(numberRolled)"
+    }
+    
+    @IBAction func alternatePlayersButtonClicked(_ sender: Any) {
+        if alternatePlayersButton == 0{
+            
+        }
+    }
 
 }
 
